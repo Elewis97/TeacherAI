7c10f8a10765ca97544202b307b52e3ebbcfe8bc
Eleanor <eleanor.lewis97@gmail.com>
2020-01-29 23:03:36 -0800

    started recording my statistics and changed things around with getJson.py

--- getJson.py
+++ getJson.py
@@ -64,12 +64,25 @@ with25 = 0
 with35 = 0
 with4 = 0
 
+total2 = 0
+total25 = 0
+total3 = 0
+total35 = 0
+total4 = 0
+
+total = 0
+bleh = 0
+same = 0
+under1 = 0
+
 allTransWords = ['first', 'second', 'also', 'finally', 'in conclusion', 'additon', 'reason']
 #maybe add... um... next and all?????
 
-for idx in range(essayNum):
+for idx in range(essayNum - 10):
     transWords = []
     # linewords = (line.split() for line in (essays[idx].lower().splitlines()))
+    paragraphNum = len(essays[idx].splitlines())
+    print(paragraphNum)
     for line in essays[idx].lower().splitlines():
         linewords = line.split()
         if len(linewords) > 1:
@@ -81,33 +94,69 @@ for idx in range(essayNum):
         for tranWord in allTransWords:
             if tranWord in words:
                 hasTransition = True
+
+    
     # print(transWords, "\n-------------------\n")
     
     splitEssay = essays[idx].lower().split()
     origLen = len(splitEssay)
-    stopwords = nltk.corpus.stopwords.words('english')
-    essayWOstopwords = [w for w in splitEssay if w not in stopwords]
-    changedLen = len(essayWOstopwords)
+    # stopwords = nltk.corpus.stopwords.words('english')
+    # essay2 = [w for w in splitEssay if w not in stopwords]
+    # englishVocab = set(w.lower() for w in nltk.corpus.words.words())
+    # misspelledWords = [w for w in essay2 if w not in englishVocab]
+
+        
+    # changedLen = len(essayWOstopwords)
+    changedLen = len(set(splitEssay))
+    # print (origLen, changedLen, (changedLen / origLen) * 100)
+    diffs = (changedLen / origLen) * 100
+
+    if leadGrades[idx] != 0.0 or endingGrades != 0.0:
+        bleh = bleh + 1
+        if (leadGrades[idx] == endingGrades[idx]):
+            same = same + 1
+        if (abs(leadGrades[idx] - endingGrades[idx]) < 1):
+            under1 = under1 + 1
+
+    if endingGrades[idx] > 0:
+        total = total + 1
     
-    if transitionGrades[idx] == 2.0:
+    if endingGrades[idx] == 2.0:
         with2 = with2 + 1
+        # total2 = total2 + origLen
+        if paragraphNum < 8:
+            total2 += 1
+        # print (with2, origLen, changedLen, (changedLen / origLen) * 100)
+        # print (misspelledWords)
         if hasTransition:
             ewith2 = ewith2 + 1
         # print("\n---------------------\n", transWords, "\n---------------------\n")
-    if transitionGrades[idx] == 2.5:
+    if endingGrades[idx] == 2.5:
         with25 = with25 + 1
+        # total25 = total25 + origLen
+        if paragraphNum < 8:
+            total25 += 1
         if hasTransition:
             ewith25 = ewith25 + 1
-    if transitionGrades[idx] == 3.0:
+    if endingGrades[idx] == 3.0:
         with3 = with3 + 1
+        # total3 = total3 + origLen
+        if paragraphNum < 8:
+            total3 += 1
         if hasTransition:
             ewith3 = ewith3 + 1
-    if transitionGrades[idx] == 3.5:
+    if endingGrades[idx] == 3.5:
         with35 = with35 + 1
+        # total35 = total35 + origLen
+        if paragraphNum < 8:
+            total35 += 1
         if hasTransition:
             ewith35 = ewith35 + 1
-    if transitionGrades[idx] == 4.0:
+    if endingGrades[idx] == 4.0:
         with4 = with4 + 1
+        # total4 = total4 + origLen
+        if paragraphNum < 8:
+            total4 += 1
         if hasTransition:
             ewith4 = ewith4 + 1
     
@@ -120,3 +169,12 @@ print ("total 3s:", with3, "trans 3s:", ewith3)
 print ("total 3.5s:", with35, "trans 3.5s:", ewith35)
 print ("total 4s:", with4, "trans 4s:", ewith4)
 
+
+print("total:", total)
+print ("2: ", total2)
+print ("2.5: ", total25)
+print ("3: ", total3)
+print ("3.5: ", total35)
+print ("4: ", total4)
+
+print ("equal:", ((same / bleh) * 100), "within 1:", (under1 / bleh) * 100)
